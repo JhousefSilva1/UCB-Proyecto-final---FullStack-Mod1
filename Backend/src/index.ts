b12363a9a124e85f14e54bbe616228e3d843b3bb
JhousefSilva1 <jhosede23@gmail.com>
2026-07-16 23:16:16 -0400

    adding last change

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -7,7 +7,7 @@ import jwt from "jsonwebtoken";
 import "dotenv/config";
 const SECRET_KEY = process.env.JWT_SECRET || "mi_clave_secreta";
 const app = express();
-const PORT = 3000;
+const PORT = 4001;
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 });
@@ -22,7 +22,7 @@ app.use(express.json());
 app.post("/login", (req: Request, res: Response) => {
   const { username, password } = req.body;
 
-  if (username === "admin" && password === "123456") {
+  if (username === "postgres" && password === "123456") {
     const token = jwt.sign(
       { username: username },
       SECRET_KEY,
